44ec40ecff6379c42851a2a4f0a033bd78071dc7
ng420 <nish.gupta01@gmail.com>
2015-04-12 15:45:12 +0530

    Added Funcs

--- symboltree.h
+++ symboltree.h
@@ -7,6 +7,17 @@ enum Type
     t_func
 };
 
+class param
+{
+	public:
+		Type t;
+		string name;
+		string genKey()
+		{
+			return name + "." + type + "." + "func";
+		}
+}
+
 class symbol
 {
 public:
@@ -14,6 +25,7 @@ public:
     std::string name, val;
     Type type;
 	int scope, param_count;
+	vector<param> paras;
     symbol(string nm, string v, Type t): name(nm), val(v), type(t) {}
     symbol(string nm): name(nm), val(string()), type(t_int) {}
 	string genKey()
@@ -22,6 +34,7 @@ public:
 		ss << scope;
 		return name + "." + type + "." + ss.str();
 	}
+	symbol(string nm, Type t, vector<param>& p): name(nm), paras(p), type(t), val(string()), param_count(p.size()) {}
 };
 
 class symtable
@@ -53,8 +66,29 @@ public:
 
 	void add_var(symbol s)
 	{
+		s.scope = scope;
 		table[scope-1].insert(make_pair(s.name, s));
 	}
 	
-	vector<string>
+	vector<string> getparams(string id)
+	{
+		symbol func = table[1][id];
+		vector<string> r;
+		for(int i = 0; i < func.param_count; ++i)
+		{
+			r.push_back(func.paras[i]);
+		}
+		return r;
+	}
+	
+	vector<string> backup()
+	{
+		vector<string> back_var;
+		for(int i = 2; i <= scope; i++)
+			for(map<string,symbol>::iterator it = table[i-1].begin(); it != table[i-1].end(); ++it)
+				back_var.push_back(it -> genKey());
+		
+		for(int i = scope; i > 1; i--)
+			table.pop_back();
+	}
 };
